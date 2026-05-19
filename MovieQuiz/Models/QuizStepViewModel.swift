5ccde9f0252f4789bc93179896bf4aed48986920
hotSpicyRamen13 <lightman40@gmail.com>
2026-05-19 19:51:12 +0300

    Начало спринта 4: создан проект MovieQuiz

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+struct QuizStepViewModel {
+    
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Ринат Чембулатов on 18.05.2026.
+//
+
